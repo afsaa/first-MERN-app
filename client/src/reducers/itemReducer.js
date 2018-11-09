b9ae573499c19f9dc1b776233d0e5fd1ad7776fe
utp <utp@utp.CRIE>
2018-11-08 20:59:49 -0500

    Finishin with Redux and Modal added

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -13,8 +13,11 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
-      return {
-        ...state
+      return { ...state };
+
+    case DELETE_ITEM:
+      return { ...state,
+        items: state.items.filter(item => item.id !== action.payload)
       };
 
     default:
